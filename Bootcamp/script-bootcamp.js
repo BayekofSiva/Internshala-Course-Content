89ddead16dde88adc3cb1f3087f1aedbbc307dbe
BayekofSiva <pratik162149@gmail.com>
2025-07-13 11:57:32 +0530

    Rename function to getPopularComments and implement filtering for comments with more than 5 likes

--- Bootcamp/script-bootcamp.js
+++ Bootcamp/script-bootcamp.js
@@ -1,13 +1,30 @@
-async function getComments() {
+async function getPopularComments() {
         try {
                 const response = await fetch('https://dummyjson.com/comments');
                 const { comments } = await response.json();
-                comments.forEach(({ body }, i) => console.log(`${i + 1}. ${body}`));
+                
+                // Filtering comments with more than 5 likes & using map to get usernames
+                const popularComments = comments
+                .filter(comment => comment.likes > 5)
+                .map(({ body, user, likes }) => ({
+                        username: user.username,
+                        body,
+                        likes
+                }));
+                
+                // Displaying the Data
+                console.log("Popular Comments (likes > 5):");
+                popularComments.forEach((comment, index) => {
+                console.log(`${index + 1}. Username: ${comment.username}`);
+                console.log(`   Comment: ${comment.body}`);
+                console.log(`   Likes: ${comment.likes}\n`);
+                });
+                
+                return popularComments;
         } 
-        catch (error) 
-        {
+        catch (error) {
                 console.error('Failed to fetch comments:', error);
         }
 }
 
-getComments();
+getPopularComments();
