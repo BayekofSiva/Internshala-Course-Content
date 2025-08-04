8a38fd03f656d7da4c50bdc26c4381a24920d5b9
BayekofSiva <pratik162149@gmail.com>
2025-08-04 16:40:12 +0530

    Task5: add NotFound page with 404 error handling and styles

--- reactassignment2/src/App.tsx
+++ reactassignment2/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BrowseBooks from './pages/BrowseBooks';
 import BookDetails from './pages/BookDetails';
+import AddBook from './pages/AddBook';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
@@ -13,7 +15,9 @@ const App = () => {
           <Route path="/books" element={<BrowseBooks />} />
           <Route path="/books/:category" element={<BrowseBooks />} />
           <Route path="/book/:id" element={<BookDetails />} />
-          {/* We'll add other routes later */}
+          <Route path="/add-book" element={<AddBook />} />
+          {/* 404 Catch-all route */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
---
+++ reactassignment2/src/pages/NotFound.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <Navbar />
+      <div className="container">
+        <h1>404 - Page Not Found</h1>
+        <p>The page you're looking for doesn't exist or has been moved.</p>
+        <Link to="/" className="home-link">Return to Home Page</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
